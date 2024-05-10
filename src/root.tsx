b4717a5951033b131ac32bfc3263de160d55d230
yuriyu000 <47517276+yuriyu000@users.noreply.github.com>
2024-05-10 16:55:02 +0900

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -18,21 +18,11 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - Bare</Title>
+        <Title>Redirecting...</Title>
         <Meta charset="utf-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+       <meta http-equiv=”refresh” content=”0;URL=’https://yu-sflabo.booth.pm/'” />
       </Head>
       <Body>
-        <Suspense>
-          <ErrorBoundary>
-            <A href="/">Index</A>
-            <A href="/about">About</A>
-            <Routes>
-              <FileRoutes />
-            </Routes>
-          </ErrorBoundary>
-        </Suspense>
-        <Scripts />
       </Body>
     </Html>
   );
